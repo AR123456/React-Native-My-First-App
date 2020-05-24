fc3679030b4a635267bbd870bc661a25458d63cc
AR123456 <coloradoz99@gmail.com>
2020-05-24 06:00:14 -0600

    another way to style

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -2,9 +2,12 @@ import React from "react";
 import { Text, StyleSheet } from "react-native";
 
 const ComponentsScreen = () => {
-  //
+  // using StyleSheet import for styleing
   return <Text style={styles.textStyle}>This is the components screen</Text>;
+  // can also use regular in line styling but if error it gives poor feedback
+  //   return <Text style={{ fontSize: 30 }}>This is the components screen</Text>;
 };
+
 const styles = StyleSheet.create({
   textStyle: {
     fontSize: 30,
