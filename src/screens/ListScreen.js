3d6c6ff3cd8facd33e77840b9b0e5aee028fde4d
AR123456 <coloradoz99@gmail.com>
2020-05-27 06:12:04 -0600

    FlatList Element

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -2,6 +2,14 @@ import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
 const ListScreen = () => {
+  // list is an array of objects that will be rendered as a seriese of screens
+  //Flat List React Native Primitive  https://reactnative.dev/docs/flatlist
+  // this is like supped up mapping from the react world ,
+  //FlatListElement turns an array into a list of elements. Pass in a prop of data -
+  //the array of data that we are going to created a bunch of elements out of.
+  // pass in a renderItem prop function that will turn each individual item into
+  // an element.
+
   return <Text>List Screen</Text>;
 };
 const styles = StyleSheet.create({});
