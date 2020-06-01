027c9f28d2b3890be7da3b06ad3c9786cb21cb3c
AR123456 <coloradoz99@gmail.com>
2020-06-01 06:27:07 -0600

    parent childe relationships

---
+++ src/components/ImageDetail.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const ImageDetail = () => {
+  return <Text>Image of forest</Text>;
+};
+const styles = StyleSheet.create({});
+export default ImageDetail;
