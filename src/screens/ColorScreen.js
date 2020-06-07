9d6a739b6dfc9e2d6ec1e9706c658aed8ded3fcd
AR123456 <coloradoz99@gmail.com>
2020-06-07 07:57:45 -0600

    add random color

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -1,28 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Button, StyleSheet } from "react-native";
-// need a state variable and ability to genrate a color randomly
 
 const ColorScreen = () => {
+  // the default state is an empty array
+  const [colors, setColors] = useState([]);
+  console.log(colors);
   return (
     <View>
-      <Button title="Add a Color" />
-      {/* create the boxes the wil display the colors 
-          hard code a style
-          */}
+      {/* onPress to update color  */}
+      <Button
+        title="Add a Color"
+        // add onPress without changeing the colors state directly,
+        // need to use the setter   setColors
+        onPress={() => {
+          // in setColors use the spread operator to get the whole color array
+          // ...gets a copy without altering, it is not destructive
+          // then call new entry of randomRgB()
+          /// why does this work too ?
+          // setColors(randomRgb());
+          setColors([...colors, randomRgb()]);
+        }}
+      />
       <View
-        //   here call randomRbg instead of hard coding
         style={{ height: 100, width: 100, backgroundColor: randomRgb() }}
       ></View>
     </View>
   );
 };
-// beneath the component definition createing a helper function to generate a random color
-// // RGB values scale from 255
+
 const randomRgb = () => {
   const red = Math.floor(Math.random() * 256);
   const green = Math.floor(Math.random() * 256);
   const blue = Math.floor(Math.random() * 256);
-  // return template string or template literal
   return `rgb(${red},${green},${blue})`;
 };
 
