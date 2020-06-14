8f88715e668dd427772a2bcaaa99abddf86bafe7
AR123456 <coloradoz99@gmail.com>
2020-06-14 05:03:05 -0600

    community conventions in reducers

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,30 +1,36 @@
-//https://www.udemy.com/course/the-complete-react-native-and-redux-course/learn/lecture/15706724#questions
+//https://www.udemy.com/course/the-complete-react-native-and-redux-course/learn/lecture/15706732#questions
+// changing code to use community conventions
 import React, { useReducer } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import ColorCounter from "../components/ColorCounter";
 
 const COLOR_INCREMENT = 35;
-const reducer = (state, action) => {
-  switch (action.colorToChange) {
-    case "red":
-      // adding validation or stop if going over 255 or below 0
-      // if (state.red + action.amount > 255 || state.red + action.amount < 0) {
-      //   return state;
-      // }
-      // using ternary expression
-      return state.red + action.amount > 255 || state.red + action.amount < 0
-        ? state
-        : { ...state, red: state.red + action.amount };
+// community conventions related to the action object in the reducer
+// they are just conventions so can be broken but others may have difficluty understanding code
+/////// what we did
+// state ==={red:number,green:number,blue:number}
+// action === {colorToChange:"red"||"green"||"blue", amount:15 || -15}
+////// changing to use community convention
+//action ===  { type: "change_red" || "change_green" || "change_blue", payload: 15 || -15 };
 
-    case "green":
-      return state.green + action.amount > 255 ||
-        state.green + action.amount < 0
+//
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "change_red":
+      return state.red + action.payload > 255 || state.red + action.payload < 0
         ? state
-        : { ...state, green: state.green + action.amount };
-    case "blue":
-      return state.blue + action.amount > 255 || state.blue + action.amount < 0
+        : { ...state, red: state.red + action.payload };
+
+    case "change_green":
+      return state.green + action.payload > 255 ||
+        state.green + action.payload < 0
         ? state
-        : { ...state, blue: state.blue + action.amount };
+        : { ...state, green: state.green + action.payload };
+    case "change_blue":
+      return state.blue + action.payload > 255 ||
+        state.blue + action.payload < 0
+        ? state
+        : { ...state, blue: state.blue + action.payload };
     default:
       return state;
   }
@@ -39,28 +45,28 @@ const SquareScreen = () => {
 
       <ColorCounter
         onIncrease={() =>
-          dispatch({ colorToChange: "red", amount: COLOR_INCREMENT })
+          dispatch({ type: "change_red", payload: COLOR_INCREMENT })
         }
         onDecrease={() =>
-          dispatch({ colorToChange: "red", amount: -1 * COLOR_INCREMENT })
+          dispatch({ type: "change_red", payload: -1 * COLOR_INCREMENT })
         }
         color="Red"
       />
       <ColorCounter
         onIncrease={() =>
-          dispatch({ colorToChange: "blue", amount: COLOR_INCREMENT })
+          dispatch({ type: "change_blue", payload: COLOR_INCREMENT })
         }
         onDecrease={() =>
-          dispatch({ colorToChange: "blue", amount: -1 * COLOR_INCREMENT })
+          dispatch({ type: "change_blue", payload: -1 * COLOR_INCREMENT })
         }
         color="Blue"
       />
       <ColorCounter
         onIncrease={() =>
-          dispatch({ colorToChange: "green", amount: COLOR_INCREMENT })
+          dispatch({ type: "change_green", payload: COLOR_INCREMENT })
         }
         onDecrease={() =>
-          dispatch({ colorToChange: "green", amount: -1 * COLOR_INCREMENT })
+          dispatch({ type: "change_green", payload: -1 * COLOR_INCREMENT })
         }
         color="Green"
       />
