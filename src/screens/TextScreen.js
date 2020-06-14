95e3ee7127928912513e911fe993001c43ae89dc
AR123456 <coloradoz99@gmail.com>
2020-06-14 05:04:33 -0600

    show text input box

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,12 +1,20 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TextInput } from "react-native";
 
 const TextScreen = () => {
   return (
     <View>
-      <Text>Text Input Box</Text>
+      {/* by default TextInput has no styling so have to add
+        that to even see it  */}
+      <TextInput style={styles.input} />
     </View>
   );
 };
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  input: {
+    margin: 15,
+    borderColor: "black",
+    borderWidth: 1,
+  },
+});
 export default TextScreen;
