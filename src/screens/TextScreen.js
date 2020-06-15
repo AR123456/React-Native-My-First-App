4f8721d5160265bba7c9542620783e1da4355495
AR123456 <coloradoz99@gmail.com>
2020-06-15 06:58:50 -0600

    autoCap and autoCorrect

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -4,9 +4,16 @@ import { View, Text, StyleSheet, TextInput } from "react-native";
 const TextScreen = () => {
   return (
     <View>
-      {/* by default TextInput has no styling so have to add
-        that to even see it  */}
-      <TextInput style={styles.input} />
+      {/* by default TextInput has no styling so have to add  that to even see input 
+        ios will by default make first letter cap and offer auto correct feature
+        android doesn't do that 
+        add 2 props to control that behaviorautoCapitalize and autoCorrect
+        here setting to not autoCorrect or capitalize    */}
+      <TextInput
+        style={styles.input}
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
     </View>
   );
 };
