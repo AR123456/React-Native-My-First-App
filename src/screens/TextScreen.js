a82c74f1770e1f560a79eeb877316d6f2c34e5b6
AR123456 <coloradoz99@gmail.com>
2020-06-16 06:56:22 -0600

    child to parent communications props

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,14 +1,21 @@
+//https://www.udemy.com/course/the-complete-react-native-and-redux-course/learn/lecture/15706772#questions
+
 import React, { useState } from "react";
 import { View, Text, StyleSheet, TextInput } from "react-native";
 
 const TextScreen = () => {
   return (
     <View>
-      {/* by default TextInput has no styling so have to add  that to even see input 
-        ios will by default make first letter cap and offer auto correct feature
-        android doesn't do that 
-        add 2 props to control that behaviorautoCapitalize and autoCorrect
-        here setting to not autoCorrect or capitalize    */}
+      {/* in react don't want the parent reaching into child element, 
+      instead if we ever want to communicate data from child up to parent 
+      need to use callback system where we pass down a prop that is a callback function 
+      and then when something happens inside the child it will call that callback and 
+      tell the parent that something just occured. 
+      That is how we communicate from child up to parent here. 
+        TextScreen element holds a piece of state that represents what is currently inside
+        the input box.  Then evertime we render text screen, will show that input element 
+        and pass in 2 props value and the onChangeText callback function
+      */}
       <TextInput
         style={styles.input}
         autoCapitalize="none"
