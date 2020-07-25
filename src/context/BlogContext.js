8782b09257360c476d68d8654912356194b76c4f
AR123456 <coloradoz99@gmail.com>
2020-07-25 05:36:30 -0600

    clean create context

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -6,30 +6,18 @@ import createDataContext from "./createDataContext";
 const blogReducer = (state, action) => {
   switch (action.type) {
     case "add_blogpost":
-      return [...state, { title: `Blog Post #${blogPosts.length + 1}` }];
-
+      return [...state, { title: `Blog Post #${state.length + 1}` }];
     default:
       return state;
   }
 };
-// this function needs access to dispatch which is in the createDataContext file
-// addBlogPost is how we change our state
-// pass in the dispatch fuction
 const addBlogPost = (dispatch) => {
-  // dont call dispatch right away put it in the return
   return () => {
-    // return a new function and inside of it return displatch
     dispatch({ type: "add_blogpost" });
   };
 };
-
-/////////////////////// in this export destructure out Context and Provider
 export const { Context, Provider } = createDataContext(
-  // then pass into createDataContext()
-  //1 the reducer
   blogReducer,
-  //2 an object with all the actions we want to have
   { addBlogPost },
-  //2 and then the initial default state value
   []
 );
--- src/context/createDataContext.js
+++ src/context/createDataContext.js
@@ -1,40 +1,21 @@
-//automating the code for reusablitly in other resouces
 import React, { useReducer } from "react";
-// pass the 3 things that need to be customized anytime we need to create a context for an addtional resource
-// 1 the reducer function itself , - reducer
-// 2 the helper function that contains the dispatch action   an object with all the call
-// back functions that can be made avalible to child components to dispatch
-// an action to change state   - actions
-// 3 inital state we call use reducer with  like an empty array, an object a number or a string.
-export default (reducer, actions, inititalState) => {
-  // create context object
+export default (reducer, actions, initialState) => {
   const Context = React.createContext();
-  // then create Provider  it takes a prop object that has a children property
+
   const Provider = ({ children }) => {
-    // call use reducer that gets back state and dispatch
-    // pass in reducer and intitialState
-    //when calling pass in reducer and intitalState
     const [state, dispatch] = useReducer(reducer, initialState);
-    /// actions === an object with key :( dispatch ) => {return ()=>{}}
-    // loop through the actions object and for every key call the dispatch function
-    // that goes into the value call in the JSX
+
     const boundActions = {};
     for (let key in actions) {
-      // key add blog post
       boundActions[key] = actions[key](dispatch);
     }
-    // then return the JSX
+
     return (
-      <Context.Provider
-        // data than need to share with rest of app is on value prop
-        // an object of whatever state is
-        // and the spread of boundActions
-        value={{ state, ...boundActions }}
-      >
+      <Context.Provider value={{ state, ...boundActions }}>
         {children}
       </Context.Provider>
     );
   };
-  // here return the Context and Provider components we just created
+
   return { Context, Provider };
 };
