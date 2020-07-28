4ce2cfcf9c6873d8590de1f881c9aeb36c50fead
AR123456 <coloradoz99@gmail.com>
2020-07-28 05:14:22 -0600

    deleting posts

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,7 +3,13 @@ import createDataContext from "./createDataContext";
 const blogReducer = (state, action) => {
   switch (action.type) {
     case "add_blogpost":
-      return [...state, { title: `Blog Post #${state.length + 1}` }];
+      return [
+        ...state,
+        {
+          id: Math.floor(Math.random() * 99999), // note there is a chance of a duplicate id Math.random
+          title: `Blog Post #${state.length + 1}`,
+        },
+      ];
     default:
       return state;
   }
