0777187b8ae69745eb4024137d54accad1e6b45b
AR123456 <coloradoz99@gmail.com>
2020-08-01 06:57:07 -0600

    getting a single post

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
 import { StyleSheet, Text, View } from "react-native";
-
-const ShowScreen = () => {
+// // note if multiple contexts are needed could do this
+// import {Context as blogContext} from "../BlogContext";
+import { Context } from "../context/BlogContext";
+// to show single item id pass cannot pass id in need to pass the
+//  navigation object
+// index screen
+const ShowScreen = ({ navigation }) => {
+  // take the getParam function provided via the navigation prop and pass in id
+  // console.log(navigation.getParam("id"));
+  // to get the list of blog posts need to use context
+  const { state } = useContext(Context);
+  // now run through the list of blog posts and find the one with the correct id
+  // using the .find array method
+  const blogPost = state.find(
+    (blogPost) => blogPost.id === navigation.getParam("id")
+  );
   return (
     <View>
-      <Text>Blog Post show screen </Text>
+      {/* pass in blogPost.title */}
+      <Text>{blogPost.title} </Text>
     </View>
   );
 };
