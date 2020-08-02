9f992b39d6db78a7200dabbcb96f60d1ecdf484f
AR123456 <coloradoz99@gmail.com>
2020-08-02 06:50:12 -0600

    local state with context use case

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,15 +1,49 @@
-import React, { useContext } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useContext, useState } from "react";
+import { View, Text, StyleSheet, TextInput, Button } from "react-native";
 import { Context } from "../context/BlogContext";
 
 const CreateScreen = ({ navigation }) => {
+  // Local State to control the text input. Even though we are making use of contex here and
+  // trying to centralize all state in one location we can still have local state
+  // inside of one single component to control the text that is being entered in the textInput
+  // The process of adding in state to the textInpo if referred to as turning it into a controlled input
+  // setting up state for  title and content
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
   return (
     <View>
-      <Text>Create</Text>
+      <Text style={styles.label}>Enter Title</Text>
+      <TextInput
+        style={styles.input}
+        value={title}
+        onChangeText={(text) => setTitle(text)}
+      ></TextInput>
+      <Text style={styles.label}>Enter Content:</Text>
+      <TextInput
+        style={styles.input}
+        value={content}
+        onChangeText={(content) => setContent(content)}
+      ></TextInput>
+      <Button title="Add Blog Post "></Button>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  input: {
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: "black",
+    marginBottom: 15,
+    padding: 5,
+    margin: 5,
+  },
+  label: {
+    fontSize: 20,
+    marginBottom: 10,
+    marginLeft: 5,
+  },
+});
 
 export default CreateScreen;
