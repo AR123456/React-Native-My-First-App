c0cba8446739b97747b6857d39bb163003cb56d5
AR123456 <coloradoz99@gmail.com>
2020-08-06 06:40:03 -0600

    edit screen initializng state from context

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,10 +1,30 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
-
+import React, { useState, useContext } from "react";
+import { View, Text, StyleSheet, TextInput } from "react-native";
+// use conext to get the blog post that needs to be edited
+import { Context } from "../context/BlogContext";
+// usethat blog to populate useState values
+// show a from where a user can edit blog post
 const EditScreen = ({ navigation }) => {
+  // reach into Context to get state
+  const { state } = useContext(Context);
+  // iterate through blog posts to find the one with the matching id
+  const blogPost = state.find(
+    (blogPost) => blogPost.id === navigation.getParam("id")
+  );
+  // initialize 2 state variables
+  // the initial state value for useState will be the found blogPost
+  const [title, setTitle] = useState(blogPost.title);
+  // same for the content of the blog post
+  const [content, setContent] = useState(blogPost.content);
+
   return (
     <View>
-      <Text>Edit Screen - {navigation.getParam("id")}</Text>
+      {/* now show the input form  */}
+      <Text>Edit Title:</Text>
+      <TextInput
+        value={title}
+        onChangeText={(newTitle) => setTitle(newTitle)}
+      />
     </View>
   );
 };
