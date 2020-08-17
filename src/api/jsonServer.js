27fc3a54e751d76a9b0856ca72098c62b7621180
AR123456 <coloradoz99@gmail.com>
2020-08-17 07:01:48 -0600

    edit a post

--- src/api/jsonServer.js
+++ src/api/jsonServer.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   // this will change every 8 hours
 
-  baseURL: "http://40b88acf118a.ngrok.io",
+  baseURL: "http://b0146467caef.ngrok.io",
 });
 // making a pre congigured version on axios
--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -4,26 +4,14 @@ import jsonServer from "../api/jsonServer";
 
 const blogReducer = (state, action) => {
   switch (action.type) {
-    // add a case for get_blogposts
     case "get_blogposts":
       return action.payload;
     case "edit_blogpost":
       return state.map((blogPost) => {
         return blogPost.id === action.payload.id ? action.payload : blogPost;
       });
-
     case "delete_blogpost":
       return state.filter((blogPost) => blogPost.id !== action.payload);
-    // case "add_blogpost":
-    //   return [
-    //     ...state,
-    //     {
-    //       id: Math.floor(Math.random() * 99999),
-    //       title: action.payload.title,
-    //       content: action.payload.content,
-    //     },
-    //   ];
-
     default:
       return state;
   }
@@ -44,11 +32,7 @@ const addBlogPost = (dispatch) => {
     }
   };
 };
-// const deleteBlogPost = (dispatch) => {
-//   return (id) => {
-//     dispatch({ type: "delete_blogpost", payload: id });
-//   };
-// };
+
 const deleteBlogPost = (dispatch) => {
   return async (id) => {
     await jsonServer.delete(`/blogposts/${id}`);
@@ -56,8 +40,21 @@ const deleteBlogPost = (dispatch) => {
   };
 };
 
+// const editBlogPost = (dispatch) => {
+//   return (id, title, content, callback) => {
+//     dispatch({
+//       type: "edit_blogpost",
+//       payload: { id, title, content },
+//     });
+//     if (callback) {
+//       callback();
+//     }
+//   };
+// };
 const editBlogPost = (dispatch) => {
-  return (id, title, content, callback) => {
+  return async (id, title, content, callback) => {
+    // as second arg tells jsonServer what to provide as updated content
+    await jsonServer.put(`/blogposts/${id}`, { title, content });
     dispatch({
       type: "edit_blogpost",
       payload: { id, title, content },
