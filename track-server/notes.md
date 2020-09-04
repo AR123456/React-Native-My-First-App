f0b33f7ae7e7f69c271c6df0aa8e3d834f4ebc43
AR123456 <coloradoz99@gmail.com>
2020-09-04 06:13:30 -0600

    signin route

--- track-server/notes.md
+++ track-server/notes.md
@@ -1,5 +1,9 @@
 ## Binder React Native
 
-## lecture 184 coding the salting and hasing
+## lecture 185 the signin route
 
-## https://www.udemy.com/course/the-complete-react-native-and-redux-course/learn/lecture/15708246#questions
+## https://www.udemy.com/course/the-complete-react-native-and-redux-course/learn/lecture/15708250#questions
+
+## using a pre hook in method in the User model to create and salt password.
+
+## now using them in the route
--- track-server/src/routes/authRoutes.js
+++ track-server/src/routes/authRoutes.js
@@ -11,15 +11,40 @@ router.post("/signup", async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = new User({ email, password });
-
     await user.save();
-
     const token = jwt.sign({ userId: user._id }, "MY_SECRET_KEY");
-
     res.send({ token });
   } catch (err) {
     return res.status(422).send(err.message);
   }
 });
 
+// post request handler
+// when someone calls the sign in route run this function
+router.post("/signin", async (req, res) => {
+  // the email and password are on rec.body
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res
+      .status(422)
+      .send({ error: "Must provide an email and password" });
+  }
+  // get user from db with the supplied email - pass email object into function.  Pass what we find into the user object
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(404).send({ error: "Invalid password or email" });
+  }
+  try {
+    // this needs to be wrapped in a try catch because if something goes wrong or passwords do not match up send message
+    // have a user so do compare using the method from the user model, pass in the password the user gave us
+    await user.comparePassword(password);
+    // we have a password match so generage a jwt
+    const token = jwt.sign({ userId: user._id }, "MY_SECRET_KEY");
+    // send back the token
+    res.send({ token });
+  } catch (err) {
+    return res.status(404).send({ error: "Invalid password or email" });
+  }
+});
 module.exports = router;
