1c5545f492cd802f1f23fb1bb28df1c274ed694f
AR123456 <coloradoz99@gmail.com>
2020-09-09 04:51:29 -0600

    Navigation hook up

--- App.js
+++ App.js
@@ -1,6 +1,41 @@
+import React from "react";
+// import {
+//   createAppContainer,
+//   createStackNavigator,
+//   createBottomTabNavigator,
+//   createSwitchNavigator,
+// } from "react-navigation";
+// import AccountScreen from "./src/screens/AccountScreen";
+// import SigninScreen from "./src/screens/SigninScreen";
+// import SignupScreen from "./src/screens/SignupScreen";
+// import TrackCreateScreen from "./src/screens/TrackCreateScreen";
+// import TrackDetailScreen from "./src/screens/TrackDetailScreen";
+// import TrackListScreen from "./src/screens/TrackListScreen";
+import { createAppContainer, createSwitchNavigator } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import { createBottomTabNavigator } from "react-navigation-tabs";
 import AccountScreen from "./src/screens/AccountScreen";
-import SinginScreen from "./src/screens/SigninScreen";
+import SigninScreen from "./src/screens/SigninScreen";
 import SignupScreen from "./src/screens/SignupScreen";
 import TrackCreateScreen from "./src/screens/TrackCreateScreen";
 import TrackDetailScreen from "./src/screens/TrackDetailScreen";
 import TrackListScreen from "./src/screens/TrackListScreen";
+const switchNavigator = createSwitchNavigator({
+  //route configuration object- bunch of nested create navigator functions
+  // list out what it will display - auth flow and not auth flow
+  // can show both Screens  and other navigators  in here
+  loginFlow: createStackNavigator({
+    Signup: SignupScreen,
+    Signin: SigninScreen,
+  }),
+  mainFlow: createBottomTabNavigator({
+    trackListFlow: createStackNavigator({
+      TrackList: TrackListScreen,
+      TrackDetail: TrackDetailScreen,
+    }),
+    TrackCreate: TrackCreateScreen,
+    Account: AccountScreen,
+  }),
+});
+
+export default createAppContainer(switchNavigator);
--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { createSwitchNavigator } from "react-navigation";
 
 const TrackListScreen = () => {
   return (
