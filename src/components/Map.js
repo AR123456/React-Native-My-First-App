f9a0722bcccce91d21df0a84e8b66234975a367d
AR123456 <coloradoz99@gmail.com>
2020-10-08 19:21:42 -0600

    starting tack create

---
+++ src/components/Map.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const Map = () => {
+  return (
+    <View>
+      <Text>I am a map</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+export default Map;
