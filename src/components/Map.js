c1c6beee8e33f98adcb5dfbd3aa78036b72695d6
AR123456 <coloradoz99@gmail.com>
2020-10-11 06:00:05 -0600

    points om map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,23 +1,37 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import MapView from "react-native-maps";
+import MapView, { Polyline } from "react-native-maps";
 
 const Map = () => {
-  // MapView is like an image and by default has no height/ width displayed
-  // by default android will show google maps - center on equator
-  // and Iphones will show apple maps- center on the country you are in
+  // fake points
+  let points = [];
+  for (let i = 0; i < 20; i++) {
+    if (i % 2 === 0) {
+      points.push({
+        latitude: 37.33233 + i * 0.001,
+        longitude: -122.03131 + i * 0.001,
+      });
+    } else {
+      points.push({
+        latitude: 37.33233 - i * 0.002,
+        longitude: -122.03131 + i * 0.001,
+      });
+    }
+  }
+
   return (
     <MapView
       style={styles.map}
       initialRegion={{
-        // what should we show initally, need lat, long, lat delta long delta
         latitude: 37.33233,
         longitude: -122.03121,
-        // delta is zoom value
         latitudeDelta: 0.01,
         longitudeDelta: 0.01,
       }}
-    ></MapView>
+    >
+      {/* Polyline takes an array of lat long objects to draw the line */}
+      <Polyline coordinates={points}></Polyline>
+    </MapView>
   );
 };
 
