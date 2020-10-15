f0fbe64e119fbb86b59ffaef8132149516e2450f
AR123456 <coloradoz99@gmail.com>
2020-10-15 06:37:51 -0600

    reading a location

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import { StyleSheet } from "react-native";
 import { Text } from "react-native-elements";
 import { SafeAreaView } from "react-navigation";
-import { requestPermissionsAsync } from "expo-location";
+import { requestPermissionsAsync,watchPositionAsync,Accuracy } from "expo-location";
 import Map from "../components/Map";
 
 const TrackCreateScreen = () => {
@@ -12,6 +12,16 @@ const TrackCreateScreen = () => {
   const startWatching = async () => {
     try {
       const { granted } = await requestPermissionsAsync();
+      await watchPositionAsync({
+        accuracy: Accuracy.BestForNavigation,
+        // on second 
+        timeInterval:1000,
+        //10 meters
+        distanceInterval:10,
+          // callback location 
+      }, (location)=>{
+        console.log(location);
+      })
       if (!granted) {
         throw new Error("Location permission not granted");
       }
