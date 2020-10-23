dfbb62054415ff0a18e30bfc2aefb6dabaeba16a
AR123456 <coloradoz99@gmail.com>
2020-10-23 06:18:26 -0600

    live location updates

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,6 @@
 import React,{useContext} from "react";
 import { ActivityIndicator, StyleSheet} from "react-native";
-import MapView, { Polyline,Circle } from "react-native-maps";
+import MapView, { Polyline } from "react-native-maps";
 import {Context as LocationContext} from "../context/LocationContext"
 
 const Map = () => {
@@ -17,20 +17,13 @@ if (!currentLocation){
         latitudeDelta: 0.01,
         longitudeDelta: 0.01,
       }}
-      // region={{
-      //   ...currentLocation.coords,
-      //   latitudeDelta: 0.01,
-      //   longitudeDelta: 0.01,
-      // }}
+      region={{
+        ...currentLocation.coords,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01,
+      }}
     >
-      <Circle
-      center={currentLocation.coords}
-      radius={50}
-      strokeColor="rgba(158,158,255,1.0)"
-      // forth value is opasity 
-      fillColor="rgba(158,158,255,0.3)"
-      
-      ></Circle>
+
     </MapView>
   );
 };
