8364466e864d66b097cc7ca893d4fa8fb91f3f82
AR123456 <coloradoz99@gmail.com>
2020-10-27 04:50:28 -0600

    location and context start recording

--- src/context/LocationContext.js
+++ src/context/LocationContext.js
@@ -3,22 +3,51 @@ import createDataContext from "./createDataContext";
 const locationReducer = (state, action) => {
   switch (action.type) {
       case "add_current_location":
-      return{ ...state, currentLocation: action.payload}    
+      return{ ...state, currentLocation: action.payload}   
+      // adding the case to start recording
+      case "start_recording" :
+      return{...state, recording:true}
+      case"stop_recording":
+      return{...state, recording:false}
+      case"add_location":
+      // add the location to the array without modifiying the original array
+      // dont mutate state !!
+      return{...state,locations:[...state.locations, action.payload] }
+      case "change_name":
+      return{...state, name: action.payload}  
+
     default:
       return state;
   }
 };
+// handleing the name of the track 
+const changeName = dispatch =>(name)=>{
+  
+  dispatch({type:"change_name", payload:name})
+}
+const startRecording = dispatch => () => {
+// when this gets called dispatch action to start recording- make recording boolean true - add this as a case in reducer
+dispatch({type: "start_recording"})
 
-const startRecording = dispatch => () => {};
-const stopRecording = dispatch => () => {};
-const addLocation = dispatch => (location) => {
-  // console.log("Hi there!")
+
+};
+const stopRecording = dispatch => () => {
+  dispatch({type: "stop_recording"})
+};
+// pass in recording argument (which is coming from TrackCreateScreen )
+const addLocation = dispatch => (location,recording) => {
+ //  if we are recording, add location to locations array
 dispatch({ type: "add_current_location", payload: location })
-
+if(recording){
+  // createding this dispatch to add a location to the locations array, make case above in the locationReducer
+  dispatch({type:"add_location", payload:location})
+}
 };
 
 export const { Context, Provider } = createDataContext(
   locationReducer,
-  { startRecording, stopRecording, addLocation },
-  { recording: false, locations: [], currentLocation: null }
+  { startRecording, stopRecording, addLocation,changeName },
+  // when recording we are adding to the locations array 
+  // keeping track of the name of the track here too - will be sending both to back end API from here so keeping them together
+  { name: "", recording: false, locations: [], currentLocation: null }
 );
--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -3,20 +3,20 @@ import "../_mockLocation.js"
 import React, { useContext } from "react";
 import { StyleSheet } from "react-native";
 import { Text } from "react-native-elements";
-// WithNavigationFocus is a higher order component - a function that we pass a component into
-// like track create - the new prop is going to tell us if the component is focused or visible on screen
 import { SafeAreaView, withNavigationFocus } from "react-navigation";
-
 import Map from "../components/Map";
 import {Context as LocationContext} from "../context/LocationContext"
 import useLocation from "../hooks/useLocation"
 import TrackForm from "../components/TrackForm"
 
 const TrackCreateScreen = ({ isFocused }) => {
-  const { addLocation } = useContext(LocationContext);
-  //passing first argument the isFocused boolean 
-  const [err] = useLocation(isFocused,addLocation);
-// console.log(isFocused)
+  const { state,addLocation } = useContext(LocationContext);
+// when passing off addLoation also pass in recording 
+ const [err]=useLocation(isFocused,(location)=>{
+   // call addLocation and the state object that has recording on it (comming from LocationContext)
+   addLocation(location,state.recording)
+ })
+
   return (
     <SafeAreaView forceInset={{ top: "always" }}>
       <Text h2>Create a Track</Text>
