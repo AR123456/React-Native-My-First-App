a02557fe5a386fbe56ac6b84d97df5bbfd771f16
AR123456 <coloradoz99@gmail.com>
2020-11-01 16:42:01 -0700

    display list of tracks

--- src/context/TrackContext.js
+++ src/context/TrackContext.js
@@ -3,7 +3,7 @@ import trackerApi from "../api/tracker"
 
 const trackReducer = (state, action) => {
     switch (action.type) {
-      case "tetch_tracks":
+      case "fetch_tracks":
         return action.payload;
       default:
         return state;
@@ -11,7 +11,7 @@ const trackReducer = (state, action) => {
   };
   
   const fetchTracks = dispatch => async () => {
-    const response = await trackerApi.get("./tracks")
+    const response = await trackerApi.get("/tracks")
     dispatch({type:"fetch_tracks", payload: response.data})
 
   };
